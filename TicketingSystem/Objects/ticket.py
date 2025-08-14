d6e24d161c6df37a12830d46631b542884194590
adroy-22 <125091060+adroy-22@users.noreply.github.com>
2025-08-13 17:48:39 -0700

    Update ticket.py, started it will finish after grabbing food.

--- TicketingSystem/Objects/ticket.py
+++ TicketingSystem/Objects/ticket.py
@@ -1,1 +1,27 @@
+from datetime import datetime
+from typing import Optional
 
+from .enums import Status, Priority
+
+class Ticket:
+  def __init__(
+    self,
+    ticketId: str,
+    title: str,
+    description: str,
+    status: Status,
+    priority: Priority,
+    isOpen: bool = true
+  ):
+    
+    self.ticket_id = ticket_id
+    self.title = title
+    self.description = description
+    self.status = status
+    self.priority = priority
+    self.created_at = datetime.now()
+    self.updated_at = datetime.now()
+    self.is_open = is_open
+    self.created_by: Optional[User] = None
+    self.assigned_to: Optional[User] = None
+  
