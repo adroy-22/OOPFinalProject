e046e22ba6bb28d928bbc136a8e6f09f2ef12b24
adroy-22 <125091060+adroy-22@users.noreply.github.com>
2025-08-13 20:03:46 -0700

    Update __init__.py

--- TicketingSystem/Objects/__init__.py
+++ TicketingSystem/Objects/__init__.py
@@ -2,4 +2,4 @@ from .enums import Status, Priority, Role
 from .ticket import Ticket
 from .user import User
 
-__all__ = ['Status', 'Priority', 'Role', 'Ticket', 'User']
+__all__ = ['Status', 'Priority', 'Ticket', 'User']
