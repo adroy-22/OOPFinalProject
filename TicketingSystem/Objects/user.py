c716dc8a3cb49b5de9bbfb147c4df1cf563ff135
adroy-22 <125091060+adroy-22@users.noreply.github.com>
2025-08-13 19:25:49 -0700

    Update user.py

--- TicketingSystem/Objects/user.py
+++ TicketingSystem/Objects/user.py
@@ -3,10 +3,21 @@ from .enums import Role
 
 class User:
     def __init__(self, user_id: str, name: str, email: str, role: Role):
-        self.user_id = user_id
-        self.name = name
-        self.email = email
-        self.role = role
-        self.tickets_created: List["Ticket"] = []
-        self.tickets_assigned: List["Ticket"] = []
-    def 
+      self.user_id = user_id
+      self.name = name
+      self.email = email
+      self.role = role
+
+      self._validate_email();
+      
+    def _validate_email(self):
+      if "@" not in self.email:
+        raise ValueError("Invalid email format")
+    
+    def to_dict(self):
+      return {
+        "user_id": self.user_id,
+        "name": self.name,
+        "email": self.email,
+        "role": self.role.value
+      }
